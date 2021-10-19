d7f3364b1fad772746db8caf74286c2db730e3b5
cloudbolt1207 <92713365+cloudbolt1207@users.noreply.github.com>
2021-10-19 11:25:12 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,6 +1,8 @@
 variable "aws_access_key" {
   type = string
+  default = "AKIAZXSDYZHGTA7CVLRT"
 }
 variable "aws_secret_key" {
   type = string
+  default = "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"
 }
